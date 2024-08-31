d3453fa2e0d0ef3be4880378b66bbf4119152f3a
trafdual <traz08102003@gmail.com>
2024-08-31 09:40:19 +0700

    f

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ app.use('/', settingsRouter)
 app.use(express.static(path.join(__dirname, '/public')))
 app.use('/public', express.static('public'));
 
-app.listen(8080, () => {
-  console.log('Server is running on port 8080')
+app.listen(3000, () => {
+  console.log('Server is running on port 3000')
   console.log(__dirname)
 })
 module.exports = app
